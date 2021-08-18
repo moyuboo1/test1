c0edcbd6590c825c4fb567c45a51394edb43660e
Moyu <moyuboo1@keio.jp>
2021-08-18 18:06:55 +0900

    test

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <mbed.h>
+
+DigitalOut pin1(D13)
+
+
+int main() {
+  while(1) {
+     pin1=1;
+     wait_us(50000)
+     pin1 =0;
+     wait_us(50000);
+
+  }
+}
+ #include <mbed.h>
+ 
+ AnalogIn name (p15);
+ DigitalOut led (LED1);
+
+ int main() {
+    while(1) {
+      if (name > ) {
+         led = 1;
+      } else {
+         led = 0;
+      }
+    }
+ }
+
+  
